01c63f58ae18e6d976309f22c41830176c7ff5a0
kwon-ys <202003339@g.seoil.ac.kr>
2024-07-10 17:23:08 +0900

    Update CategoryControllerTest.java

--- myjpa/src/test/java/com/kys/myjpa/category/CategoryControllerTest.java
+++ myjpa/src/test/java/com/kys/myjpa/category/CategoryControllerTest.java
@@ -27,12 +27,12 @@ public class CategoryControllerTest {
         assertThat(responseInsert).isNotNull();
         assertThat(responseInsert.getStatusCode()).isEqualTo(HttpStatus.BAD_REQUEST);
 
-        CategoryDto requestInsert2 = CategoryDto.builder().name("RestFulAPIInput").build();
+        CategoryDto requestInsert2 = CategoryDto.builder().name("RestFulAPI Input").build();
         ResponseEntity<CategoryDto> responseInsert2 = this.testRestTemplate.postForEntity(url + "/ct"
                 ,requestInsert2, CategoryDto.class);
         assertThat(responseInsert2).isNotNull();
         assertThat(responseInsert2.getStatusCode()).isEqualTo(HttpStatus.OK);
         System.out.println("responseInsert2.getBody().getId() = " + responseInsert2.getBody().getId());
-        assertThat(responseInsert2.getBody().getName()).isEqualTo("RestFulAPIInput");
+        assertThat(responseInsert2.getBody().getName()).isEqualTo("RestFulAPI Input");
     }
 }
